724a7f93ab99cab16fee42da2e02b9837ba45f7a
Jason Ballejo <ballejo19@gmail.com>
2022-07-08 23:44:34 -0400

    more updates

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+/*
 const h1 = document.createElement("h1");
 h1.textContent = "Join my journey!";
 
-document.querySelector("body").appendChild(h1);
+document.querySelector("body").appendChild(h1); 
+*/
+
+function appearNormal() {
+    document.getElementById('profile').src="Test.jpeg";
+}
+
+function description() {
+    document.getElementById('profile').src="html-5.png";
+}
+
+document.getElementById('profile').addEventListener('mouseout', appearNormal);
+document.getElementById('profile').addEventListener('mouseover', description);
